a398a8aa25bb9a917297a4dbb825a35a78dcb3cd
crefixtech <84495476+crefixtech@users.noreply.github.com>
2021-05-20 12:21:53 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$database_username="nutrimen_hopon";
-$database_password="Hopon@bhaskar";
+$database_username="db_username";
+$database_password="db_password";
 $server_name="localhost";
-$database_name1="nutrimen_hopon";
+$database_name1="db_name";
 
 $conn1=mysqli_connect($server_name,$database_username,$database_password,$database_name1);
 ?>
